011213fdf6fd6ae0de8e86ed151719eca076fffb
d.smirnov <d.smirnov@s-e.su>
2018-09-25 14:27:32 +0300

    use data class

--- app/src/main/java/com/android/example/currencyconverter/model/entity/Currency.kt
+++ app/src/main/java/com/android/example/currencyconverter/model/entity/Currency.kt
@@ -2,18 +2,4 @@ package com.android.example.currencyconverter.model.entity
 
 import java.math.BigDecimal
 
-class Currency(val title: String, val value: BigDecimal) {
-    override fun equals(other: Any?): Boolean {
-        if (other is Currency) {
-            return title == other.title && value == other.value
-        } else {
-            return false
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = title.hashCode()
-        result = 31 * result + value.hashCode()
-        return result
-    }
-}
+data class Currency(val title: String, val value: BigDecimal)
